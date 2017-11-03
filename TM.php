8dd67b6a1d6b3704cf0f7814a30a78ebdf3aeaae
tesis20 <33339379+tesis20@users.noreply.github.com>
2017-11-02 22:45:01 -0600

    Add files via upload

---
+++ TM.php
@@ -0,0 +1,40 @@
+<html>
+<head>
+<title>Operaciones Basicas</title>
+</head>
+<body>
+<blockquote>
+  <blockquote>
+    <blockquote>
+      <h2>Tablas de Multiplicar</h2>
+    </blockquote>
+  </blockquote>
+</blockquote>
+<form action="../../../Users/Alejandro/Downloads/tablas.php" method="POST">
+  <table>
+  
+  <td height="5">
+  <td width="548"><input type="submit" name="1" value="Tabla del uno">
+    <input type="submit" name="2" value="Tabla del Dos">
+    <input type="submit" name="3" value="Tabla del Tres">
+<input type="submit" name="4" value="Tabla del Cuatro">
+<input type="submit" name="5" value="Tabla del Cinco">
+<img src="imagen/fondo.jpg" width="493" height="362" alt=""/><BR>
+<BR>
+<input type="submit" name="6" value="Tabla del Seis">
+<input type="submit" name="7" value="Tabla del Siete">
+<input type="submit" name="8" value="Tabla del Ocho">
+<input type="submit" name="9" value="Tabla del Nueve">
+<input type="submit" name="10" value="Tabla del Diez">
+<BR>
+<BR>
+<BR>
+<img src="imagen/fondo.jpg" width="493" height="362" alt=""/><BR>
+<BR><BR><BR><BR>
+
+
+</td>
+</table>
+</form>
+</body>
+</html>
---
+++ tablas.php
@@ -0,0 +1,132 @@
+<?php
+
+if ($_POST['1']) { 
+echo "Tabla del uno"."<BR>";
+echo "1 x 1 = 1"."<BR>"; 
+echo "1 x 2 = 2"."<BR>";
+echo "1 x 3 = 3"."<BR>";
+echo "1 x 4 = 4"."<BR>";
+echo "1 x 5 = 5"."<BR>";
+echo "1 x 6 = 6"."<BR>";
+echo "1 x 7 = 7"."<BR>";
+echo "1 x 8 = 8"."<BR>";
+echo "1 x 9 = 9"."<BR>";
+echo "1 x 10 = 10";
+} 
+else if ($_POST['2']) {  ; 
+echo "Tabla del Dos"."<br>";
+echo "2 x 1 = 2"."<br>";
+echo "2 x 2 = 4"."<br>";
+echo "2 x 3 = 6"."<br>";
+echo "2 x 4 = 8"."<br>";
+echo "2 x 5 =10"."<br>";
+echo "2 x 6 =12"."<br>";
+echo "2 x 7 =14"."<br>";
+echo "2 x 8 =16"."<br>";
+echo "2 x 9 =18"."<br>";
+echo "2 x 10 = 20"; 
+}
+if ($_POST['3']) {  
+ echo "Tabla del tres"."<br>";
+echo "3 x 1 = 3"."<br>";
+echo "3 x 2 = 6"."<br>";
+echo "3 x 3 = 9"."<br>";
+echo "3 x 4 =12"."<br>";
+echo "3 x 5 =15"."<br>";
+echo "3 x 6 =18"."<br>";
+echo "3 x 7 =21"."<br>";
+echo "3 x 8 =24"."<br>";
+echo "3 x 9 =27"."<br>";
+echo "3 x 10 = 30"; 
+}
+else if ($_POST['4']) {  
+echo "Tabla del cuatro"."<br>";
+echo "4 x 1 = 4"."<br>";
+echo "4 x 2 = 8"."<br>";
+echo "4 x 3 =12"."<br>";
+echo "4 x 4 =16"."<br>";
+echo "4 x 5 =20"."<br>";
+echo "4 x 6 =24"."<br>";
+echo "4 x 7 =28"."<br>";
+echo "4 x 8 =32"."<br>";
+echo "4 x 9 =36"."<br>";
+echo "4 x 10 = 40"; 
+}
+if ($_POST['5']) { 
+echo "Tabla del Cinco"."<br>";
+echo "5 x 1 = 5"."<br>";
+echo "5 x 2 =10"."<br>";
+echo "5 x 3 =15"."<br>";
+echo "5 x 4 =20"."<br>";
+echo "5 x 5 =25"."<br>";
+echo "5 x 6 =30"."<br>";
+echo "5 x 8 =40"."<br>";
+echo "5 x 9 =45"."<br>";
+echo "5 x 10 = 50";
+}
+else if ($_POST['6']) {  
+echo "Tabla del Seis"."<br>";
+echo "6 x 1 = 6"."<br>";
+echo "6 x 2 =12"."<br>";
+echo "6 x 3 =18"."<br>";
+echo "6 x 4 =24"."<br>";
+echo "6 x 5 =30"."<br>";
+echo "6 x 6 =36"."<br>";
+echo "6 x 7 =42"."<br>";
+echo "6 x 8 =48"."<br>";
+echo "6 x 9 =54"."<br>";
+echo "6 x 10 = 60";
+}
+if ($_POST['7']) {  
+echo "Tabla del Siete"."<br>";
+echo "7 x 1 = 7"."<br>";
+echo "7 x 2 =14"."<br>";
+echo "7 x 3 =21"."<br>";
+echo "7 x 4 =28"."<br>";
+echo "7 x 5 =35"."<br>";
+echo "7 x 6 =42"."<br>";
+echo "7 x 7 =49"."<br>";
+echo "7 x 8 =56"."<br>";
+echo "7 x 9 =63"."<br>";
+echo "7 x 10 = 70";
+}
+else if ($_POST['8']) {  
+echo "Tabla del Ocho"."<br>";
+echo "8 x 1 = 8"."<br>";
+echo "8 x 2 =16"."<br>";
+echo "8 x 3 =24"."<br>";
+echo "8 x 4 =32"."<br>";
+echo "8 x 5 =40"."<br>";
+echo "8 x 6 =48"."<br>";
+echo "8 x 7 =56"."<br>";
+echo "8 x 8 =64"."<br>";
+echo "8 x 9 =72"."<br>";
+echo "8 x 10 = 80";
+}
+if ($_POST['9']) {  
+echo "Tabla del Nueve"."<br>";
+echo "9 x 1 = 9"."<br>";
+echo "9 x 2 =18"."<br>";
+echo "9 x 3 =27"."<br>";
+echo "9 x 4 =36"."<br>";
+echo "9 x 5 =45"."<br>";
+echo "9 x 6 =54"."<br>";
+echo "9 x 7 =63"."<br>";
+echo "9 x 8 =72"."<br>";
+echo "9 x 9 =81"."<br>";
+echo "9 x 10 = 90";
+}
+else if ($_POST['10']) {  
+echo "Tabla del Diez"."<br>";
+echo "10 x 1 =10"."<br>";
+echo "10 x 2 =20"."<br>";
+echo "10 x 3 =30"."<br>";
+echo "10 x 4 =40"."<br>";
+echo "10 x 5 =50"."<br>";
+echo "10 x 6 =60"."<br>";
+echo "10 x 7 =70"."<br>";
+echo "10 x 8 =80"."<br>";
+echo "10 x 9 =90"."<br>";
+echo "10 x 10 = 100";
+}
+?>
